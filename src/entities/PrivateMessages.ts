dbbfbf2de56f1220e97301e616cc3defb804acb6
eddy madu <eddymadu90@gmail.com>
2022-05-24 14:20:26 +0100

    added private chat model and the oneToMany  relationship

---
+++ src/entities/PrivateMessages.ts
@@ -0,0 +1,32 @@
+import {
+       Column, Entity, ObjectIdColumn, ObjectID, CreateDateColumn, UpdateDateColumn, BaseEntity, ManyToMany, ManyToOne, JoinColumn
+} from "typeorm"
+
+import { User } from "./User";
+
+@Entity("private-message")
+export class PrivateMessage extends BaseEntity {
+       @ObjectIdColumn({ generated: true })
+       id!: ObjectID;
+
+       @Column()
+       message!: string
+       @ManyToOne(() => User,
+              (user) => { user.messages }
+       )
+
+       @JoinColumn({ name: "messsage-sender" })
+       sender!: User
+
+       @JoinColumn({ name: "messsage-reciever" })
+       reciever!: User
+
+       @CreateDateColumn()
+       created_at!: Date
+
+       @UpdateDateColumn()
+       updatedDate!: Date
+
+
+
+}
--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,62 +1,87 @@
 import {
-       Column, Entity, ObjectIdColumn, ObjectID, CreateDateColumn, UpdateDateColumn, BeforeInsert, BaseEntity
+       Column, Entity, ObjectIdColumn, ObjectID, CreateDateColumn, UpdateDateColumn, BeforeInsert, BaseEntity, OneToOne, OneToMany
 } from "typeorm"
-import { Contains, IsInt, Length, IsEmail, IsFQDN, IsDate, Min, Max, MinLength } from "class-validator";
+import { IsEmail, MinLength } from "class-validator";
 import * as bcrypt from 'bcryptjs';
 
 import endPoint from "../config/endpoints.config"
+import { PrivateMessage } from "./PrivateMessages";
 
+// define enum
+enum UserEnum {
+       USER = "USER",
+       ADMIN = "ADMIN",
+       MODERATOR = "MODERATOR"
+
+}
 @Entity("user")
 export class User extends BaseEntity {
        @ObjectIdColumn({ generated: true })
-       public id!: ObjectID;
+       id!: ObjectID;
+
        @Column({ type: "varchar" })
        @MinLength(3)
-       public firstName!: string;
+       firstName!: string;
+
        @Column({ type: "varchar" })
        @MinLength(3)
-       public lastName!: string;
+       lastName!: string;
+
        @Column({ select: false })
-       public password!: string;
+       password!: string;
+
        @Column({ unique: true })
        @IsEmail()
-       public email!: string;
+       email!: string;
+
        @Column({ type: "boolean", default: false })
-       public isLoggedIn!: boolean;
-       @Column({ type: "varchar", enum: ["USER", "ADMIN", "MODERATOR"], default: "USER" })
-       public role!: string;
+       isLoggedIn!: boolean;
+       @Column({ type: "enum", enum: UserEnum, default: "USER" })
+       role!: string;
        @Column({ nullable: true })
-       public profilePicture!: string;
-       @Column({ nullable: true })
-       public address!: string;
+       profilePicture!: string;
+
        @Column({ type: "int", nullable: true })
-       public mobile!: number;
+       mobile!: number;
+
        @Column({ type: "int" })
        zipcode!: number;
-       @Column({ type: "simple-json", nullable: true, name: "user-address-details" })
-       public location!: {
-              streetName: string
+
+       @Column({ type: "geometry", nullable: true, name: "user-address-details" })
+       location!: {
+              coordinates: number[]
+              formattedAddress: string
+              street: string
               city: string
               streetNumber: number
               country: string
               countryCode: string
-              latitude: string
-              longitude: string
+              state: string
+
        }
+
+       @Column({ nullable: true })
+       access_token!: string
+
+       @OneToMany(() => PrivateMessage,
+              (message) => message.sender
+
+       )
+       messages!: PrivateMessage
+
        @CreateDateColumn()
        created_at!: Date
 
        @UpdateDateColumn()
        updatedDate!: Date
-       @Column({ nullable: true })
-       access_token!: string
+
+
        @BeforeInsert()
        async beforeInsert() {
               this.password = await bcrypt.hash(this.password, endPoint.bycriptHashRound);
               console.log(this.password)
-              if (this.address != null) {
-                     console.log(this.address)
-              }
+
+
        }
 
 }
