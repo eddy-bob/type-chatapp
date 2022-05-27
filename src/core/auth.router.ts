c3cb0a9d8bfeb6e9b029fd539f38dfd83297f4bd
eddy madu <eddymadu90@gmail.com>
2022-05-27 22:23:02 +0100

    feat:completed auth  apis

--- src/core/auth.router.ts
+++ src/core/auth.router.ts
@@ -4,7 +4,7 @@ const authRouter = Router();
 import secure from "../middlewares/secure"
 authRouter.post("/register", auth.register)
 authRouter.post("/login", auth.login)
-authRouter.post("/logout", auth.logout)
+authRouter.get("/logout", auth.logout)
 authRouter.put("/update-password", secure, auth.updatePassword)
 authRouter.post("/forgot-password/init", auth.forgotPasswordInit)
 authRouter.put("/:token/reset-password", auth.forgotPasswordComplete)
