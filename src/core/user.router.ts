48b17263a101643efe38898e6c282349787d1016
eddy madu <eddymadu90@gmail.com>
2022-06-02 16:55:42 +0100

    fix:fixed issue of role auth tsc compilation error

--- src/core/user.router.ts
+++ src/core/user.router.ts
@@ -5,9 +5,9 @@ import secure from "../middlewares/secure"
 import authorize from "../middlewares/authorize"
 
 userRouter.use(secure as any)
-userRouter.get("/users", authorize("ADMIN"), user.getUsers)
+userRouter.get("/users", authorize(["ADMIN"]), user.getUsers)
 userRouter.get("/user-profile/:id", user.getUser)
-userRouter.delete("/delete-all", authorize("ADMIN"), user.deleteUsers)
+userRouter.delete("/delete-all", authorize(["ADMIN"]), user.deleteUsers)
 userRouter.delete("/delete-account", user.deleteUser)
 userRouter.put("/update-user", user.updateUser)
 
--- src/middlewares/authorize.ts
+++ src/middlewares/authorize.ts
@@ -2,9 +2,13 @@ import { Request, Response, NextFunction } from "express"
 import { customError } from "../helpers/customError"
 
 interface Authorize extends Request { userRole: string }
-const authorize = ([...roles], req: Authorize, res: Response, next: NextFunction) => {
-       if (roles.includes(req.userRole)) { next() }
-       else { return next(new customError("You are forbidden to access this route", 403)) }
+const authorize = ([...roles]: string[]): any => {
 
+
+       return (req: Authorize, res: Response, next: NextFunction) => {
+              if (roles.includes(req.userRole)) { next() }
+              else { return next(new customError("You are forbidden to access this route", 403)) }
+
+       }
 }
 export default authorize
