1974b08cf3b106187e66bfe2cb98580f10f48d0f
eddy madu <eddymadu90@gmail.com>
2022-06-02 17:47:57 +0100

    fix:auth router compilation error

--- src/core/auth.router.ts
+++ src/core/auth.router.ts
@@ -5,11 +5,11 @@ import secure from "../middlewares/secure"
 authRouter.post("/register", auth.register)
 authRouter.post("/login", auth.login)
 authRouter.get("/logout", auth.logout)
-authRouter.put("/update-password", secure, auth.updatePassword)
+authRouter.put("/update-password", secure as any, auth.updatePassword)
 authRouter.post("/forgot-password/init", auth.forgotPasswordInit)
 authRouter.put("/:token/reset-password", auth.forgotPasswordComplete)
-authRouter.get("/verify-email/init", secure, auth.verifyEmail)
-authRouter.get(`/verify-email/:token/confirm`, secure, auth.checkVerifyEmailToken)
+authRouter.get("/verify-email/init", secure as any , auth.verifyEmail)
+authRouter.get(`/verify-email/:token/confirm`, secure as any, auth.checkVerifyEmailToken)
 
 
 
