4f8db79a201cd8822886b83c518852cc0d936c35
eddybob <eddymadu90@gmail.com>
2022-10-11 20:55:21 +0100

    fix:commited key

--- src/app.ts
+++ src/app.ts
@@ -38,6 +38,7 @@ app.use((req, res, next) => {
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
 
+  
   // Request headers you wish to allow
   res.setHeader(
     "Access-Control-Allow-Headers",
