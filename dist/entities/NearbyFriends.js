1dfbc599242ed4406ee698c3b08c2c89e9ac9037
eddybob <eddymadu90@gmail.com>
2022-10-12 20:34:16 +0100

    fix:added profile,deleted dist and added a start script

--- dist/entities/NearbyFriends.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=NearbyFriends.js.map
--- dist/utils/socket-auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const validateToken_1 = require("../api/validateToken");
-const socketAuth = (token) => {
-    const response = (0, validateToken_1.validateToken)(token);
-    return response;
-};
-exports.default = socketAuth;
-//# sourceMappingURL=socket-auth.js.map
