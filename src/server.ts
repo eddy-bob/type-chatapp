32d39489bcc315c67adef96c17664ab0ad6b7efe
eddybob <eddymadu90@gmail.com>
2022-10-12 20:12:50 +0100

    fix:added consoles as well as removed extra params in ther server callback

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,36 @@
-import { app, server as appServer } from "./app"
+import { app, server as appServer } from "./app";
 import "reflect-metadata";
 import { appendFileSync } from "fs";
 
-import { exit } from 'process';
-import { Request, Response, NextFunction } from "express"
-import * as path from "path"
-const port = app.get("port")
-const mode = app.get("enviroment")
-
+import { exit } from "process";
+import { Request, Response, NextFunction } from "express";
+import * as path from "path";
+const port = app.get("port");
+const mode = app.get("enviroment");
 
 const server = (): void => {
+  const myApp = appServer.listen(port, () => {
+    console.log(`server running on port ${port} in mode ${mode}`.blue);
+  });
 
-       const myApp = appServer.listen(port, "0.0.0.0", () => { console.log(`server running on port ${port} in mode ${mode}`.blue) });
+  process.on("unhandledRejection", function (reason: Error) {
+    // write error to file
+    appendFileSync(
+      "error.txt",
+      `\n Error: ${new Date(Date.now())} ${reason.message}`
+    );
 
-       process.on('unhandledRejection', function (reason: Error) {
+    if (reason.message.startsWith("querySrv ESERVFAIL _mongodb._tcp")) {
+      myApp.close(() => {
+        console.log((reason.name + ":", reason.message).underline.red);
+        console.log("server closed".red);
 
-              // write error to file
-              appendFileSync("error.txt", `\n Error: ${new Date(Date.now())} ${reason.message}`)
+        exit(1);
+      });
+    } else {
+      console.log(reason.message.red);
+    }
+  });
+};
 
-
-              if (reason.message === "querySrv ESERVFAIL _mongodb._tcp.nodechatapp.khmbi.mongodb.net") {
-                     myApp.close(() => {
-                            console.log((reason.name + ":", reason.message).underline.red);
-                            console.log("server closed".red)
-
-                            exit(1)
-
-                     })
-              }
-              else { console.log(reason.message.red) }
-
-       });
-}
-
-server()
+server();
