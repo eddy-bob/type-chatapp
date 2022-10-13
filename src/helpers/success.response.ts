49261ed1de804a926eaaa5e3cf80a4f1b62b0126
eddybob <eddymadu90@gmail.com>
2022-10-13 12:13:55 +0100

    added consoles to track movement

--- src/helpers/success.response.ts
+++ src/helpers/success.response.ts
@@ -1,6 +1,7 @@
 import { Response } from "express"
 const successResponse = (response: Response, data: Object | undefined, statusCode: number, message: string, access_token?: string): void => {
        if (access_token) {
+              console.log("it entered here")
               response.status(statusCode).json({ data, message, access_token })
        } else { response.status(statusCode).json({ data, message }) }
 
